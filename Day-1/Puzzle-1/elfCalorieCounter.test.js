7a55fb33e01d4d7e26ea435a40ceeb810988a403
ritaaktay <ritaaktay@gmail.com>
2022-12-01 18:40:11 +0000

    Changed folder structure

--- Day-1/Puzzle-1/elfCalorieCounter.test.js
+++ Day-1/Puzzle-1/elfCalorieCounter.test.js
@@ -2,7 +2,7 @@ const ElfCalorieCounter = require("./elfCalorieCounter.js");
 
 describe("CalorieCounter", () => {
   it("Has a calories list", () => {
-    const path = "./Day-1/mock.txt";
+    const path = "./Day-1/Puzzle-1/mock.txt";
     const elfCalCounter = new ElfCalorieCounter(path);
     expect(elfCalCounter.getElfWithMostCalories()).toEqual(24000);
   });
