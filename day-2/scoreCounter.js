6e041676703db2aca7840c39e70987cc8bb9c9aa
ritaaktay <ritaaktay@gmail.com>
2022-12-02 17:09:17 +0000

    Passed test for example input

--- day-2/scoreCounter.js
+++ day-2/scoreCounter.js
@@ -3,10 +3,30 @@ const { readFileSync } = require("fs");
 class ScoreCounter {
   constructor(path) {
     this.path = path;
+    this.legend = {
+      "A X": 3,
+      "A Y": 0,
+      "A Z": 6,
+      "B Y": 3,
+      "B X": 6,
+      "B Z": 0,
+      "C Z": 3,
+      "C X": 0,
+      "C Y": 6,
+      "A": 1,
+      "B": 2,
+      "C": 3,
+    };
   }
 
   count() {
-    console.log(this.splitRounds());
+    return this.splitRounds()
+      .map((round) => this.getScore(round))
+      .reduce((a, b) => a + b);
+  }
+
+  getScore(round) {
+    return this.legend[round[0]] + this.legend[round];
   }
 
   splitRounds() {
