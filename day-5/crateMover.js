1c112bba7c7d8f897b6b07b8da927fb33ff3cb62
ritaaktay <ritaaktay@gmail.com>
2022-12-06 19:14:30 +0000

    Makes stacks object with keys as stack number and values as arrays containing crates as letters

--- day-5/crateMover.js
+++ day-5/crateMover.js
@@ -3,15 +3,35 @@ const { readFileSync } = require("fs");
 class CrateMover {
   constructor(path) {
     this.path = path;
-    this.crates = this.getCrates();
+    this.stacks = this.getStacks();
     this.instructions = this.getInstructions();
   }
 
-  getCrates() {
+  getStacks() {
     const lines = readFileSync(this.path).toString().split("\n");
     lines.pop();
     const divider = lines.indexOf("");
-    console.log(lines.slice(0, divider));
+    const crateData = lines.slice(0, divider - 1);
+    return this.makeStacks(crateData);
+  }
+
+  makeStacks(crateData) {
+    const stackAmount = (crateData[0].length + 1) / 4;
+    let stacks = {};
+    for (let i = 1; i <= stackAmount; i++) {
+      stacks[i] = [];
+    }
+    crateData.forEach((row) => {
+      let counter = 1;
+      for (let i = 1; i < row.length; i += 4) {
+        if (row[i] != " ") {
+          stacks[counter].push(row[i]);
+        }
+        counter++;
+        if (counter > stackAmount) counter = 0;
+      }
+    });
+    return stacks;
   }
 
   getInstructions() {
--- day-5/crateMover.test.js
+++ day-5/crateMover.test.js
@@ -3,8 +3,8 @@ const CrateMover = require("./CrateMover");
 describe("CrateMover", () => {
   it("has instructions array", () => {
     path = "./day-5/mock.txt";
-    const counter = new CrateMover(path);
-    expect(counter.instructions).toEqual([
+    const mover = new CrateMover(path);
+    expect(mover.instructions).toEqual([
       "move 1 from 2 to 1",
       "move 3 from 1 to 3",
       "move 2 from 2 to 1",
@@ -12,14 +12,13 @@ describe("CrateMover", () => {
     ]);
   });
 
-  it("has crates array", () => {
+  it("has stacks object", () => {
     path = "./day-5/mock.txt";
-    const counter = new CrateMover(path);
-    expect(counter.instructions).toEqual([
-      "move 1 from 2 to 1",
-      "move 3 from 1 to 3",
-      "move 2 from 2 to 1",
-      "move 1 from 1 to 2",
-    ]);
+    const mover = new CrateMover(path);
+    expect(mover.stacks).toEqual({
+      "1": ["N", "Z"],
+      "2": ["D", "C", "M"],
+      "3": ["P"],
+    });
   });
 });
