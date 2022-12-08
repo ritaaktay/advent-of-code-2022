6a115c884cd896f074f65f9790d3c5a7987ace84
ritaaktay <ritaaktay@gmail.com>
2022-12-08 07:36:04 +0000

    Passes Part 1 of Day 6

--- day-6/communicationDevice.js
+++ day-6/communicationDevice.js
@@ -2,18 +2,13 @@ const { readFileSync } = require("fs");
 
 class CommunicationDevice {
   findStartOfPacketMarker(data) {
-    console.log({ data });
     let different = data[0];
-    console.log({ different });
     for (let i = 1; i < data.length; i++) {
-      console.log({ i });
-      console.log(!different.includes(data[i]));
       if (different.includes(data[i])) {
         const same = different.indexOf(data[i]);
         different = different.substr(same + 1);
       }
       different += data[i];
-      console.log({ different });
       if (different.length == 4) return i + 1;
     }
   }
@@ -21,6 +16,6 @@ class CommunicationDevice {
 
 const data = readFileSync("./day-6/input.txt").toString();
 const device = new CommunicationDevice();
-// console.log(device.findStartOfPacketMarker(data));
+console.log(device.findStartOfPacketMarker(data));
 
 module.exports = CommunicationDevice;
