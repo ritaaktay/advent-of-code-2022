00322a073a1510645a6655c3f818a7768ecdf4f6
ritaaktay <ritaaktay@gmail.com>
2022-12-08 07:32:57 +0000

    Green step for findins start of package marker

--- day-6/communicationDevice.js
+++ day-6/communicationDevice.js
@@ -1,11 +1,27 @@
 const { readFileSync } = require("fs");
 
 class CommunicationDevice {
-  findStartOfPacketMarker(data) {}
+  findStartOfPacketMarker(data) {
+    console.log({ data });
+    let different = data[0];
+    console.log({ different });
+    for (let i = 1; i < data.length; i++) {
+      console.log({ i });
+      console.log(!different.includes(data[i]));
+      if (!different.includes(data[i])) {
+        different += data[i];
+      } else {
+        const same = different.indexOf(data[i]);
+        different = different.substr(same + 1) + data[i];
+      }
+      console.log({ different });
+      if (different.length == 4) return i + 1;
+    }
+  }
 }
 
 const data = readFileSync("./day-6/input.txt").toString();
 const device = new CommunicationDevice();
-console.log(device.findStartOfPacketMarker(data));
+// console.log(device.findStartOfPacketMarker(data));
 
 module.exports = CommunicationDevice;
