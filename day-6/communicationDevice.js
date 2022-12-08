d0fa0fbadaa16d41d23d1bd009bf16082429e282
ritaaktay <ritaaktay@gmail.com>
2022-12-08 07:44:48 +0000

    Passed part two tests by adding a single digit :))))

--- day-6/communicationDevice.js
+++ day-6/communicationDevice.js
@@ -12,6 +12,18 @@ class CommunicationDevice {
       if (different.length == 4) return i + 1;
     }
   }
+
+  findStartOfMessageMarker(data) {
+    let different = data[0];
+    for (let i = 1; i < data.length; i++) {
+      if (different.includes(data[i])) {
+        const same = different.indexOf(data[i]);
+        different = different.substr(same + 1);
+      }
+      different += data[i];
+      if (different.length == 14) return i + 1;
+    }
+  }
 }
 
 const data = readFileSync("./day-6/input.txt").toString();
--- day-6/communicationDevice.test.js
+++ day-6/communicationDevice.test.js
@@ -1,38 +1,73 @@
 const CommunicationDevice = require("./communicationDevice");
 
 describe("CommunicationDevice", () => {
-  it("Reports characters until first start of packet marker", () => {
+  xit("Reports characters until first start of packet marker", () => {
     const device = new CommunicationDevice();
     expect(
       device.findStartOfPacketMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
     ).toEqual(7);
   });
 
-  it("Reports characters until first start of packet marker", () => {
+  xit("Reports characters until first start of packet marker", () => {
     const device = new CommunicationDevice();
     expect(
       device.findStartOfPacketMarker("bvwbjplbgvbhsrlpgdmjqwftvncz")
     ).toEqual(5);
   });
 
-  it("Reports characters until first start of packet marker", () => {
+  xit("Reports characters until first start of packet marker", () => {
     const device = new CommunicationDevice();
     expect(
       device.findStartOfPacketMarker("nppdvjthqldpwncqszvftbrmjlhg")
     ).toEqual(6);
   });
 
-  it("Reports characters until first start of packet marker", () => {
+  xit("Reports characters until first start of packet marker", () => {
     const device = new CommunicationDevice();
     expect(
       device.findStartOfPacketMarker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
     ).toEqual(10);
   });
 
-  it("Reports characters until first start of packet marker", () => {
+  xit("Reports characters until first start of packet marker", () => {
     const device = new CommunicationDevice();
     expect(
       device.findStartOfPacketMarker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
     ).toEqual(11);
+  });
+
+  it("Reports characters until first start of message marker", () => {
+    const device = new CommunicationDevice();
+    expect(
+      device.findStartOfMessageMarker("mjqjpqmgbljsphdztnvjfqwrcgsmlb")
+    ).toEqual(19);
+  });
+
+  it("Reports characters until first start of message marker", () => {
+    const device = new CommunicationDevice();
+    expect(
+      device.findStartOfMessageMarker("bvwbjplbgvbhsrlpgdmjqwftvncz")
+    ).toEqual(23);
+  });
+
+  it("Reports characters until first start of message marker", () => {
+    const device = new CommunicationDevice();
+    expect(
+      device.findStartOfMessageMarker("nppdvjthqldpwncqszvftbrmjlhg")
+    ).toEqual(23);
+  });
+
+  it("Reports characters until first start of message marker", () => {
+    const device = new CommunicationDevice();
+    expect(
+      device.findStartOfMessageMarker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")
+    ).toEqual(29);
+  });
+
+  it("Reports characters until first start of message marker", () => {
+    const device = new CommunicationDevice();
+    expect(
+      device.findStartOfMessageMarker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")
+    ).toEqual(26);
   });
 });
