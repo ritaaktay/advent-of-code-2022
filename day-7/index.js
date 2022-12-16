ef6777e836546b046ab198d1849db0148e39c4d9
ritaaktay <ritaaktay@gmail.com>
2022-12-16 15:25:05 +0000

    Passes Part 2 of Day 7

--- day-7/index.js
+++ day-7/index.js
@@ -3,4 +3,4 @@ const DirectoryCreator = require("./directoryCreator");
 
 const calculator = new DirectoryCalculator();
 const root = new DirectoryCreator().create("./day-7/input.txt");
-console.log(calculator.calculate(root));
+console.log(calculator.pickSmallestAbove(root, 70000000, 30000000));
