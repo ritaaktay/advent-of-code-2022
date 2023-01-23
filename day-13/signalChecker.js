d82fa22e545621b7ab6a83db24a5fb484eb20199
ritaaktay <ritaaktay@gmail.com>
2023-01-23 19:51:29 +0000

    Parses input for Day 13

--- day-13/signalChecker.js
+++ day-13/signalChecker.js
@@ -4,11 +4,48 @@ class SignalChecker {
   constructor(path) {
     this.path = path;
     this.data = this.parse();
+    console.log(this.data);
+  }
+
+  isInOrder(left, right) {
+    if (right.length < left.length) return false;
   }
 
   parse() {
-    const lines = readFileSync(this.path).toString().split("\n");
-    lines.pop();
+    const pairs = [];
+    const lines = readFileSync(this.path)
+      .toString()
+      .split("\n")
+      .filter((i) => i != "");
+    for (let i = 0; i < lines.length; i += 2) {
+      pairs.push([lines[i], lines[i + 1]]);
+    }
+    return pairs.map((p) => p.map((s) => this.makeArray(s)));
+  }
+
+  getSubString(str, start) {
+    let match = 1;
+    for (let i = start + 1; i < str.length; i++) {
+      if (str[i] == "[") match++;
+      if (str[i] == "]") match--;
+      if (match == 0) return str.substring(start, i + 1);
+    }
+  }
+
+  makeArray(str) {
+    const array = [];
+    let i = 1;
+    while (i < str.length - 1) {
+      if (str[i] == "[") {
+        const sub = this.getSubString(str, i);
+        array.push(this.makeArray(sub));
+        i = i + sub.length;
+      } else {
+        if (str[i] != ",") array.push(parseInt(str[i]));
+        i++;
+      }
+    }
+    return array;
   }
 }
 
--- day-13/signalChecker.test.js
+++ day-13/signalChecker.test.js
@@ -1,5 +1,20 @@
 const SignalChecker = require("./SignalChecker");
 
 describe("", () => {
-  it("", () => {});
+  it("", () => {
+    const checker = new SignalChecker("./day-13/mock.txt");
+  });
+
+  it("gets substrings", () => {
+    const checker = new SignalChecker("./day-13/mock.txt");
+    expect(checker.getSubString("[[1],[2,3,4]]", 5)).toEqual("[2,3,4]");
+    expect(checker.getSubString("[1,[2,[3,[4,[5,6,7]]]],8,9]", 3)).toEqual(
+      "[2,[3,[4,[5,6,7]]]]"
+    );
+  });
+
+  it("parses nested arrays", () => {
+    const checker = new SignalChecker("./day-13/mock.txt");
+    expect(checker.makeArray("[[1],[2,3,4]]", 5)).toEqual([[1], [2, 3, 4]]);
+  });
 });
