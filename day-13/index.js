f66d876a918b6d77215b6a5376e4f0c4ee3c7461
ritaaktay <ritaaktay@gmail.com>
2023-01-26 12:12:09 +0000

    Updated README

--- day-13/index.js
+++ day-13/index.js
@@ -1,4 +1,3 @@
 const SignalChecker = require("./signalChecker");
 const checker = new SignalChecker("./day-13/input.txt");
 console.log("Part 1: ", checker.countRightPairs());
-// console.log("Part 2: ", );
