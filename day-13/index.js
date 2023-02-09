4c2548b413aed4e10e89854c39b2ef635a21a28a
ritaaktay <ritaaktay@gmail.com>
2023-02-09 11:28:46 +0000

    Passes puzzle input for Part 2 of Day 13

--- day-13/index.js
+++ day-13/index.js
@@ -1,3 +1,4 @@
 const SignalChecker = require("./signalChecker");
 const checker = new SignalChecker("./day-13/input.txt");
 console.log("Part 1: ", checker.countRightPairs());
+console.log("Part 2: ", checker.findDividers());
