a449e2f1e55b430f7fb7e03447fd94ab40ad0bf3
ritaaktay <ritaaktay@gmail.com>
2023-02-09 11:26:51 +0000

    Passes test input for Part 2 of Day 13

--- day-13/signalChecker.js
+++ day-13/signalChecker.js
@@ -6,32 +6,46 @@ class SignalChecker {
     this.data = this.parse();
   }
 
+  findDividers() {
+    const list = this.data.flat(1).concat([[[2]], [[6]]]);
+    const sorted = list.sort((a, b) => this.isInOrder(a, b));
+    let result = 1;
+    sorted.forEach((item, index) => {
+      if (JSON.stringify(item) === JSON.stringify([[2]])) result *= index + 1;
+      if (JSON.stringify(item) === JSON.stringify([[6]])) result *= index + 1;
+    });
+    return result;
+  }
+
   countRightPairs() {
     let sum = 0;
     this.data.forEach((pair, i) => {
       const [l, r] = pair;
-      if (this.isInOrder(l, r)) sum += i + 1;
+      if (this.isInOrder(l, r) == -1) sum += i + 1;
     });
     return sum;
   }
 
   isInOrder(l, r) {
     if (typeof l == "number" && typeof r == "number") {
-      if (l > r) return false;
-      if (l < r) return true;
+      if (l > r) return 1;
+      if (l < r) return -1;
     }
 
     if (Array.isArray(l) || Array.isArray(r)) {
       if (typeof l == "number") l = [l];
       if (typeof r == "number") r = [r];
 
-      while (l.length && r.length) {
-        const result = this.isInOrder(l.shift(), r.shift());
-        if (typeof result == "boolean") return result;
+      const length = l.length > r.length ? r.length : l.length;
+
+      for (let i = 0; i < length; i++) {
+        const result = this.isInOrder(l[i], r[i]);
+        if (result == 1 || result == -1) return result;
       }
 
-      if (l.length) return false;
-      if (r.length) return true;
+      if (l.length > r.length) return 1;
+      if (r.length > l.length) return -1;
+      return 0;
     }
   }
 
--- day-13/signalChecker.test.js
+++ day-13/signalChecker.test.js
@@ -3,16 +3,21 @@ const SignalChecker = require("./SignalChecker");
 describe("", () => {
   it("compares a pair", () => {
     const checker = new SignalChecker("./day-13/mock.txt");
-    expect(checker.isInOrder([1, 1, 3, 1, 1], [1, 1, 5, 1, 1])).toEqual(true);
+    expect(checker.isInOrder([1, 1, 3, 1, 1], [1, 1, 5, 1, 1])).toEqual(-1);
   });
 
   it("compares a pair", () => {
     const checker = new SignalChecker("./day-13/mock.txt");
-    expect(checker.isInOrder([[1], [2, 3, 4]], [[1], 4])).toEqual(true);
+    expect(checker.isInOrder([[1], [2, 3, 4]], [[1], 4])).toEqual(-1);
   });
 
   it("count pairs in right order", () => {
     const checker = new SignalChecker("./day-13/mock.txt");
     expect(checker.countRightPairs()).toEqual(13);
+  });
+
+  it("finds dividers", () => {
+    const checker = new SignalChecker("./day-13/mock.txt");
+    expect(checker.findDividers()).toEqual(140);
   });
 });
