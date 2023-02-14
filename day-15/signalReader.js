b3a6e386c48a55e02c97fd7c58ebae63b46eb9af
ritaaktay <ritaaktay@gmail.com>
2023-02-14 11:14:52 +0000

    Day 15 Part 1 refactor

--- day-15/signalReader.js
+++ day-15/signalReader.js
@@ -4,21 +4,28 @@ class SignalReader {
   constructor(path, y) {
     this.path = path;
     this.y = y;
-    [this.row, this.scanners] = this.parse();
+    this.data = this.parse();
+    // [this.row, this.scanners] = this.parse();
   }
 
   calculate() {
-    const scanned = this.scanners
-      .map((s) => this.getScan(s))
+    const scanned = this.getScannersInMaxRange()
+      .map((s) => this.getScanRange(s))
       .filter((r) => r != 0);
-    const joined = this.concat(scanned);
-    const occupied = this.getOccupied(joined);
-    const amounts = joined.map((range) => range[1] - range[0]);
-    return amounts.reduce((a, b) => a + b, 0) + 1 - occupied;
-    // the plus one is for having the start & end indices inclusive
+    const joined = this.joinRanges(scanned);
+    const beacons = this.getBeaconCount(joined);
+    const lengths = joined.map((range) => range[1] - range[0]);
+    return lengths.reduce((a, b) => a + b, 0) + 1 - beacons;
   }
 
-  getScan(s) {
+  getScannersInMaxRange() {
+    const max = Math.max(...this.data.map((c) => c[4]));
+    return this.data.filter(
+      (c) => c[1] <= this.y + max && c[1] >= this.y - max
+    );
+  }
+
+  getScanRange(s) {
     const range = s[4];
     const distance = Math.abs(this.y - s[1]);
     const amount = range * 2 + 1 - distance * 2;
@@ -27,7 +34,7 @@ class SignalReader {
     return [start, start + amount - 1];
   }
 
-  concat(ranges) {
+  joinRanges(ranges) {
     const concatenated = [ranges[0]];
     for (let i = 1; i < ranges.length; i++) {
       concatenated.forEach((c, x) => {
@@ -36,17 +43,14 @@ class SignalReader {
         else concatenated.push(ranges[i]);
       });
     }
-    if (this.allAreSeparate(concatenated)) return concatenated;
-    return this.concat(concatenated);
+    if (this.noOverlaps(concatenated)) return concatenated;
+    return this.joinRanges(concatenated);
   }
 
-  allAreSeparate(ranges) {
-    // returns false if it finds overlap there is overlap
-    // returns true if there is no overlap between any of the ranges
+  noOverlaps(ranges) {
     if (ranges.length == 1) return true;
     for (let i = 0; i < ranges.length; i++) {
       for (let x = 0; x < ranges.length; x++) {
-        // DO NOT COMPARE SAME INDICES
         if (i == x) continue;
         if (this.overlap(ranges[i], ranges[x])) return false;
       }
@@ -55,9 +59,6 @@ class SignalReader {
   }
 
   overlap(a, b) {
-    // where a and b are arrays with [start,end]
-    // returns false is no overlap
-    // returns joined indices [start, end] if overlap
     const [firstStart, firstEnd] = a[0] > b[0] ? b : a;
     const [secondStart, secondEnd] = a[0] <= b[0] ? b : a;
     if (firstEnd <= secondEnd && firstEnd >= secondStart) {
@@ -67,16 +68,21 @@ class SignalReader {
     return false;
   }
 
-  getOccupied(ranges) {
-    const blocked = this.row.map((x) => {
+  getBeaconCount(ranges) {
+    const beacons = [];
+    this.getRow().forEach((x) => {
       for (let i = 0; i < ranges.length; i++) {
-        const start = ranges[i][0];
-        const end = ranges[i][1];
-        if (start < x[0] && end > x[0] && x[1] == this.y) return x[0];
-        if (start < x[2] && end > x[2] && x[3] == this.y) return x[2];
+        if (ranges[i][0] < x[2] && ranges[i][1] > x[2] && x[3] == this.y) {
+          beacons.push(x[2]);
+          return;
+        }
       }
     });
-    return [...new Set(blocked)].length;
+    return [...new Set(beacons)].length;
+  }
+
+  getRow() {
+    return this.data.filter((c) => c[1] == this.y || c[3] == this.y);
   }
 
   parse(y) {
@@ -92,12 +98,14 @@ class SignalReader {
     coordinates.forEach((c) => {
       c.push(Math.abs(c[2] - c[0]) + Math.abs(c[3] - c[1]));
     });
-    const max = Math.max(...coordinates.map((c) => c[4]));
-    const scanners = coordinates.filter(
-      (c) => c[1] <= this.y + max && c[1] >= this.y - max
-    );
-    const row = coordinates.filter((c) => c[1] == this.y || c[3] == this.y);
-    return [row, scanners];
+    return coordinates;
+
+    // const max = Math.max(...coordinates.map((c) => c[4]));
+    // const scanners = coordinates.filter(
+    //   (c) => c[1] <= this.y + max && c[1] >= this.y - max
+    // );
+    // const row = coordinates.filter((c) => c[1] == this.y || c[3] == this.y);
+    // return [row, scanners];
   }
 }
 
